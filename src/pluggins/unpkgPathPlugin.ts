3070b945df08e03c04d51c3e9e6bd2a40ad336c8
Tom Verkon <tverkon@usa.net>
2021-02-09 17:44:46 -0500

    implemented caching using localForage

--- src/pluggins/unpkgPathPlugin.ts
+++ src/pluggins/unpkgPathPlugin.ts
@@ -1,10 +1,23 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'fileCache',
+});
+
+// (async () => {
+//   await fileCache.setItem('color', 'red');
+//   const color = await fileCache.getItem('color');
+//   console.log(color);
+// })()
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
+
     setup(build: esbuild.PluginBuild) {
+
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
         if (args.path === 'index.js') {
@@ -32,17 +45,26 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('nested-test-pkg');
-              console.log(message);
+              import React, {useState} from 'react-select'
+              console.log(React, useState);
             `,
           };
         }
+
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+
         const { data, request } = await axios.get(args.path);
-        return {
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
-        }
+        };
+        await fileCache.setItem(args.path, result);
+        return result;
       });
     },
   };
