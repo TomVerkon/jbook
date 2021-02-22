d3b75fb9a8e370dd831f4975bb02065116d31dea
Tom Verkon <tverkon@usa.net>
2021-02-22 11:28:09 -0500

    cleaning up css for horizontal and vertical resizing

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import { ResizableBox } from 'react-resizable';
+import React, { FC, useEffect, useState } from 'react';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
 
 interface ResizableProps {
@@ -7,17 +7,42 @@ interface ResizableProps {
 }
 
 const Resizable: FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox
-      minConstraints={[Infinity, 24]}
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      height={300}
-      width={Infinity}
-      resizeHandles={['s']}
-    >
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const listener = () => {
+      setInnerHeight(window.innerHeight);
+      setInnerWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', listener);
+    // cleanup for when the component will stop being displayed or removed from the screen
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
+      height: Infinity,
+      width: innerWidth * 0.75,
+      resizeHandles: ['e'],
+    };
+  } else {
+    resizableProps = {
+      minConstraints: [Infinity, 24],
+      maxConstraints: [Infinity, innerHeight * 0.9],
+      height: 300,
+      width: Infinity,
+      resizeHandles: ['s'],
+    };
+  }
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
