fe62031c62f00a21b10e391562d18b2207e0c0b3
Tom Verkon <tverkon@usa.net>
2021-03-01 07:27:00 -0500

    Add Cell functionality

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTypedSelector } from '../state/useTypedSelector';
 import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) => {
@@ -10,10 +11,18 @@ const CellList: React.FC = () => {
   });
 
   const renderedCells = cells.map((cell) => (
-    <CellListItem key={cell.id} cell={cell} />
+    <React.Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </React.Fragment>
   ));
 
-  return <div>{renderedCells}</div>;
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
