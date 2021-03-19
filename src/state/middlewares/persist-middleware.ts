60cdc37c4f3719e5b0b8f04c397f0e1eebc6bf06
Tom Verkon <tverkon@usa.net>
2021-03-19 13:55:18 -0400

    added debounce logic

--- src/state/middlewares/persist-middleware.ts
+++ src/state/middlewares/persist-middleware.ts
@@ -5,11 +5,17 @@ import { saveCells } from '../action-creators';
 import { RootState } from '../reducers';
 
 export const persistMiddleware = ({ dispatch, getState }: { dispatch: Dispatch<Action>; getState: () => RootState }) => {
+  let timer: any;
   return (next: (action: Action) => void) => {
     return (action: Action) => {
       next(action);
       if ([ActionType.MOVE_CELL, ActionType.UPDATE_CELL, ActionType.DELETE_CELL, ActionType.INSERT_CELL_AFTER].includes(action.type)) {
-        saveCells()(dispatch, getState);
+        if (timer) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+          saveCells()(dispatch, getState);
+        }, 250);
       }
     };
   };
